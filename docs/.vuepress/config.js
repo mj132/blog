01290dda70e3569e46818d743727bb267ccf7e09
“majia <1329442936@qq.com>
2020-06-08 19:06:51 +0800

    初始

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  base: "/blog/",
+  title: '大前端',
+  description: '前端技术博客，记录成长轨迹',
+  themeConfig: {
+    nav: [{
+        text: "主页",
+        link: "/"
+      },
+      {
+        text: "前端",
+        link: "/webframe/"
+      },
+      // {
+      //   text: "node",
+      //   link: "/node/"
+      // },
+      {
+        text: "面试问题",
+        link: "/interview/"
+      }
+    ]
+  },
+  sidebar: 'auto',
+  extraWatchFiles: [
+    './README.md'
+  ]
+}
---
+++ docs/README.md
@@ -0,0 +1,13 @@
+---
+home: true
+heroImage: '/img/clock.gif'
+actionText: 开始阅读 →
+actionLink: /webframe/
+features:
+- title: day day up
+  details: 记录每一天的进步, 一分耕耘，一分收获。
+- title: MJ
+  details: 一个有趣的且乐于分享的人。座右铭：今日事今日毕。
+footer: MJ personal blog
+
+---
