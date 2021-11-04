a7b607e83a74a5bf72ffe3690627c9678fbcfe98
马佳 <majia@jybdgroup.cn>
2021-11-04 17:36:06 +0800

    vue3

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -223,7 +223,18 @@ module.exports = {
         ["vue/vue3_vue2", "Vue3有了解过吗？能说说跟Vue2的区别吗？"]
       ],
     },
-  ], 
+    {
+      title: '面试题-vue3',
+      children: [
+        ["vue3/goal", "Vue3.0的设计目标是什么？做了哪些优化?"],
+        ["vue3/performance", "Vue3.0 性能提升主要是通过哪几方面体现的？"],
+        ["vue3/proxy", "Vue3.0里为什么要用 Proxy API 替代 defineProperty API ？"],
+        ["vue3/composition", "Vue3.0 所采用的 Composition Api 与 Vue2.x 使用的 Options Api 有什么不同？"],
+        ["vue3/treeshaking", "说说Vue 3.0中Treeshaking特性？举例说明一下？"],
+        ["vue3/modal_component", "用Vue3.0 写过组件吗？如果想实现一个 Modal你会怎么设计？"],
+      ]
+    },
+  ],
   '/node/': [
     {
       title: 'node是什么',
